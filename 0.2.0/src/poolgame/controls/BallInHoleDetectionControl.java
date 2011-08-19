8e94200b6847f9083931ce2d8dad5c45c1b84f6b
sunshinegamestudio <sunshinegamestudio@9ea8aea3-5048-2ed5-aec5-4ad8660567a1>
2011-08-19 20:47:03 +0000

    V 0.2.0.

---
+++ 0.2.0/src/poolgame/controls/BallInHoleDetectionControl.java
@@ -0,0 +1,49 @@
+/*
+ * To change this template, choose Tools | Templates and open the template in
+ * the editor.
+ */
+package poolgame.controls;
+
+import com.jme3.bullet.PhysicsSpace;
+import com.jme3.bullet.PhysicsTickListener;
+import com.jme3.bullet.collision.PhysicsCollisionEvent;
+import com.jme3.bullet.collision.PhysicsCollisionListener;
+import com.jme3.bullet.collision.shapes.MeshCollisionShape;
+import com.jme3.bullet.control.RigidBodyControl;
+import com.jme3.scene.Mesh;
+import com.jme3.scene.control.Control;
+
+/**
+ *
+ * @author Vortex
+ */
+
+public class BallInHoleDetectionControl extends RigidBodyControl implements PhysicsTickListener, PhysicsCollisionListener   {
+//public class BallInHoleDetectionControl extends Control implements PhysicsTickListener, PhysicsCollisionListener   {
+    
+    private MeshCollisionShape meshCollisionShape;
+    
+    public void BallInHoleDetectionControl(Mesh hole)    {
+        meshCollisionShape = new MeshCollisionShape(hole);
+        
+        super.setMass(0);
+        super.setCollisionShape(collisionShape);
+        getPhysicsSpace().addTickListener(this);
+        getPhysicsSpace().addCollisionListener(this);
+    }
+    public void prePhysicsTick(PhysicsSpace space, float f){
+      // apply state changes ...
+    }
+
+    public void physicsTick(PhysicsSpace space, float f){
+      // poll game state ...
+    }
+
+    public void collision(PhysicsCollisionEvent event) {
+        if ("Box".equals(event.getNodeA().getName()) || "Box".equals(event.getNodeB().getName())) {
+            if ("bullet".equals(event.getNodeA().getName()) || "bullet".equals(event.getNodeB().getName())) {
+                //fpsText.setText("You hit the box!");
+            }
+        }
+    }
+}

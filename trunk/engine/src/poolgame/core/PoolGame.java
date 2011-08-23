47955794b4ccbfd600aa6db77e9c27a5dd5c6975
sunshinegamestudio <sunshinegamestudio@9ea8aea3-5048-2ed5-aec5-4ad8660567a1>
2011-08-23 20:02:15 +0000

    First implementation for logging.

--- trunk/engine/src/poolgame/core/PoolGame.java
+++ trunk/engine/src/poolgame/core/PoolGame.java
@@ -25,12 +25,17 @@ import com.jme3.bullet.BulletAppState.ThreadingType;
 import com.jme3.bullet.PhysicsSpace;
 import com.jme3.bullet.PhysicsSpace.BroadphaseType;
 import com.jme3.math.Vector3f;
+import com.jme3.renderer.Caps;
 import com.jme3.renderer.RenderManager;
 import com.jme3.renderer.ViewPort;
 import com.jme3.system.AppSettings;
 import com.jme3.system.JmeSystem;
 import com.jme3.system.Timer;
 
+import java.util.Collection;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import poolgame.appstates.MainMenuState;
 import poolgame.appstates.InGameMenuState;
 import poolgame.appstates.GameState;
@@ -47,6 +52,8 @@ public class PoolGame extends Application {
 	private MainMenuState ms = null;
 	private InGameMenuState is = null;
         private BulletAppState bulletAppState = null;
+        private Logger logger = Logger.getLogger(PoolGame.class.getName());
+        //private FileHandler fh = new FileHandler("mylog.txt");
 
         static PoolGame thisApp;
 	
@@ -62,18 +69,30 @@ public class PoolGame extends Application {
         //return pSpace;
         return bulletAppState.getPhysicsSpace();
     }
+    
+    public Logger getLogger()
+    {
+        return logger;
+    }
 
     @Override
     public void start(){
+        getLogger().addHandler(fh);
+        
         // set some default settings in-case
         // settings dialog is not shown
         if (settings == null)
             setSettings(new AppSettings(true));
 
         // show settings dialog
-        if (!JmeSystem.showSettingsDialog(settings, false))
-        	return;
+        if (!JmeSystem.showSettingsDialog(settings, false)) {
+            getLogger().log(Level.SEVERE, "jME system initialisation error");
+            return;
+        }
 
+        //Collection<Caps> caps = renderer.getCaps();
+        //getLogger().log(Level.SEVERE, "Caps: {0}" + caps.toString());
+        
         super.start();
     }
 	

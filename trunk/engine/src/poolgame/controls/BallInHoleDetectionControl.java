3f1f5d022b36c1f6647a70d8c5ef59b324e73f8f
sunshinegamestudio <sunshinegamestudio@9ea8aea3-5048-2ed5-aec5-4ad8660567a1>
2011-11-13 21:47:52 +0000

    Detect if the ball collides with the bottom plate of the hole.

--- trunk/engine/src/poolgame/controls/BallInHoleDetectionControl.java
+++ trunk/engine/src/poolgame/controls/BallInHoleDetectionControl.java
@@ -8,26 +8,29 @@ import com.jme3.bullet.PhysicsSpace;
 import com.jme3.bullet.PhysicsTickListener;
 import com.jme3.bullet.collision.PhysicsCollisionEvent;
 import com.jme3.bullet.collision.PhysicsCollisionListener;
+import com.jme3.bullet.collision.shapes.BoxCollisionShape;
 import com.jme3.bullet.collision.shapes.MeshCollisionShape;
+import com.jme3.bullet.control.GhostControl;
 import com.jme3.bullet.control.RigidBodyControl;
+import com.jme3.math.Vector3f;
 import com.jme3.scene.Mesh;
 import com.jme3.scene.control.Control;
 
 /**
  *
  * @author Vortex
+ * Detect if the ball collides with the bottom plate of the hole.
  */
 
 public class BallInHoleDetectionControl extends RigidBodyControl implements PhysicsTickListener, PhysicsCollisionListener   {
-//public class BallInHoleDetectionControl extends Control implements PhysicsTickListener, PhysicsCollisionListener   {
     
-    private MeshCollisionShape meshCollisionShape;
+    private BoxCollisionShape boxCollisionShape;
     
-    public void BallInHoleDetectionControl(Mesh hole)    {
-        meshCollisionShape = new MeshCollisionShape(hole);
+    public void BallInHoleDetectionControl(Mesh hole)   {
+        boxCollisionShape = new BoxCollisionShape(new Vector3f(1, 1, 1));
         
         super.setMass(0);
-        super.setCollisionShape(collisionShape);
+        super.setCollisionShape(boxCollisionShape);
         getPhysicsSpace().addTickListener(this);
         getPhysicsSpace().addCollisionListener(this);
     }
